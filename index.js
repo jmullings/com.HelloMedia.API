16359b8b653888bc6513c34cf0a1dfbd36bdab86
jmullings <j_mullings@hotmail.co.uk>
2017-05-26 20:30:01 +0800

    live mongoDB

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var hds = require('express-handlebars');
+
+var index = require('./routes/index');
+
 var app = express();
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
 
-app.set('port', (process.env.PORT || 5000));
+// Connection URL
+var url = 'mongodb://localhost:27017/nodeTut';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+  db.close();
 });
 
+
+// view engine setup
+app.engine('hds', hds({extname: 'hds', defaultlayout:'layout',layoutsdir:__dirname + '/views/layouts' }));
+app.set('port', (process.env.PORT || 5000));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hds');
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+module.exports = app;
 
